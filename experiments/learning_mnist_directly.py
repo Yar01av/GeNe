70e71e88626639371905584999ec4739a50b6c2e
Yaroslav Nazarov <yaroslavn99@gmail.com>
2022-08-08 21:03:02 +0200

    More experiments

--- experiments/learning_mnist_directly.py
+++ experiments/learning_mnist_directly.py
@@ -7,6 +7,7 @@ from torchvision import datasets
 from torchvision.transforms import transforms
 from tqdm import tqdm
 
+from gene.optimisers.annealed_crossing import AnnealedCrossingOptimiser
 from gene.optimisers.division import ParallelDivisionOptimiser, DivisionOptimiser
 import torch
 import torch.nn.functional as F
@@ -16,13 +17,12 @@ from gene.targets import get_negative_accuracy_target
 from gene.util import get_accuracy
 
 DEVICE = ["cpu", "cuda"][1]
-N_EPOCHS = 10
-
+N_EPOCHS = 50
 
 # Define the model
 model = torch.nn.Sequential(
     torch.nn.Flatten(),
-    torch.nn.Linear(28*28, 256),
+    torch.nn.Linear(28 * 28, 256),
     torch.nn.ReLU(),
     torch.nn.Linear(256, 64),
     torch.nn.ReLU(),
@@ -31,37 +31,39 @@ model = torch.nn.Sequential(
 models = [model.to(DEVICE)]
 
 # Define the optimiser
-optimiser = ParallelDivisionOptimiser(target_func=get_negative_accuracy_target,
-                              random_function=lambda shape: torch.normal(0, 0.1, shape),
-                              selection_limit=10,
-                              multi_proc_batch_size=18,
-                              device=DEVICE)
+optimiser = AnnealedCrossingOptimiser(target_func=get_negative_accuracy_target,
+                                      init_std=4.4402,
+                                      std_updater=lambda std: std * 0.999 if std >= 0.0001 else std,
+                                      max_couples=10,
+                                      n_children_per_couple=2,
+                                      selection_limit=10,
+                                      device=DEVICE)
 
 # Define the data
 train_data = datasets.MNIST(
-        root="./cache",
-        download=True,
-        train=True,
-        transform=transforms.ToTensor()
+    root="./cache",
+    download=True,
+    train=True,
+    transform=transforms.ToTensor()
 )
 train_loader = DataLoader(train_data, batch_size=1024, shuffle=True)
 test_data = datasets.MNIST(
-        root="./cache",
-        download=True,
-        train=False,
-        transform=transforms.ToTensor()
-    )
+    root="./cache",
+    download=True,
+    train=False,
+    transform=transforms.ToTensor()
+)
 test_loader = DataLoader(test_data, batch_size=1024)
 
 latest_scores = []
 
-for e in tqdm(range(N_EPOCHS)):
-    for images, labels in tqdm(train_loader, leave=False):
+for e in tqdm(range(N_EPOCHS), leave=False):
+    for images, labels in train_loader:
         models = optimiser.step(models, images.to(DEVICE), labels.to(DEVICE))
 
-        latest_score = np.mean([get_accuracy(test_loader, m, DEVICE) for m in models])
-        latest_scores.append(latest_score)
-        print(latest_score)
+    latest_score = np.mean([get_accuracy(test_loader, m, DEVICE) for m in models])
+    latest_scores.append(latest_score)
+    print(latest_score)
 
 end = datetime.now()
 
